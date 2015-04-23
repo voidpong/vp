655352059a0db7e57e56011932726723a4074928
Sergio <sata@ic.ufal.br>
2015-04-23 15:01:49 -0300

    Inclusão do arquivo de instalação (executar como root)

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir -p /usr/share/vpong
+gcc $(pwd)/main.c -o /usr/share/vpong/vpong
+cp $(pwd)/ico.jpg /usr/share/vpong/ico.png
+ln -sf /usr/share/vpong/vpong /usr/bin/vpong
+echo "[Desktop Entry]
+Version=0.1.9
+Name=Void Pong
+Exec=vpong
+Icon=/usr/share/vpong/ico.png
+Terminal=true
+Type=Application
+Categories=Utility;Application;" > /usr/share/applications/vpong.desktop
+chmod 777 "/usr/share/applications/vpong.desktop"

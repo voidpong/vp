a004ff5d0e47f4fdffae0b0d06a7aeab4a959f22
Sergio Amorim <sata@ic.ufal.br>
2015-04-23 00:59:57 -0300

    Função main() (primeira versão)
    Primeira versão da função main, apenas para chamar funções de exibição

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "getch.h" // funcao para receber a tecla sem o usuario digitar enter
+#include "funcoes_void.h" //funcoes de exibicao, como o menu
+int main (){
+	while (1){ //executa o menu a nao ser que o usuario digite S ou s (linha 17), e retorna 0. 
+		menu(); 
+		switch (getch()){
+			case 32: printf("CHAMAR JOGO\n"); //32=espaço
+					getch();
+				break;
+			case 73:
+			case 105: instrucoes(); //73='I' e 105='i'
+				break;
+			case 67:
+			case 99: creditos(); //67='C' e 99='c'
+				break;
+			case 83: 
+			case 115: return 0;// 83='S' e 115='s'
+		}
+	}
+}

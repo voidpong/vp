b1a4362afe8835946e2e15fa09d6caa0f16f3991
Sergio Amorim <sata@ic.ufal.br>
2015-05-12 10:15:03 -0300

    Inclusão da função gotoxy()

--- imprimir.h
+++ imprimir.h
@@ -34,6 +34,46 @@ void centralizar_vertical(int n){ // recebe o numero de linhas que serao impress
 	for (i=0;i<w.ws_row/2-n/2;i++)
 		printf("\n");
 }
+//FUNÇÃO PARA MUDAR O CURSOR PARA UMA DETERMINADA POSIÇÃO NO TERMINAL
+void gotoxy(int x, int y) {
+        char essq[100];     /* String variable to hold the escape sequence */
+        char xstr[100];     /* Strings to hold the x and y coordinates */
+        char ystr[100];     /* Escape sequences must be built with characters */
+ 
+        /*
+        ** Convert the screen coordinates to strings
+        */
+        sprintf(xstr, "%d", x);
+        sprintf(ystr, "%d", y);
+ 
+        /*
+        ** Build the escape sequence (vertical move)
+        */
+        essq[0] = '\0';
+        strcat(essq, "\033[");
+        strcat(essq, ystr);
+ 
+        /*
+        ** Described in man terminfo as vpa=\E[%p1%dd
+        ** Vertical position absolute
+        */
+        strcat(essq, "d");
+ 
+        /*
+        ** Horizontal move
+        ** Horizontal position absolute
+        */
+        strcat(essq, "\033[");
+        strcat(essq, xstr);
+        /* Described in man terminfo as hpa=\E[%p1%dG */
+        strcat(essq, "G");
+ 
+        /*
+        ** Execute the escape sequence
+        ** This will move the cursor to x, y
+        */
+        printf("%s", essq);
+}
 /*
 void main (){
 	puts ("String à esquerda!");
